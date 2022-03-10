698ee524742fee2701e3f1f534538c1042ee260c
DariaHub <pomelova.dasha@mail.ru>
2022-03-10 20:59:08 +0300

    Добавьте файлы проекта.

---
+++ Zadanie2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Текстик (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileInfo fileInfo = new FileInfo(openFileDialog.FileName);
+
+                StreamReader reader = new StreamReader(fileInfo.Open(FileMode.Open, FileAccess.Read), Encoding.GetEncoding(1251));
+
+                tbx.Text = reader.ReadToEnd();
+
+                reader.Close();
+                return;
+            }
+        }
+        //Сохранение файла
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt (*.txt)|*.txt";
+
+            if (saveFileDialog1.ShowDialog() == true)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.OpenFile(), System.Text.Encoding.Default))
+                {
+                    sw.Write(tbx.Text);
+                    sw.Close();
+                }
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Zadanie7/AnswerGen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie7
+{
+    class AnswerGen
+    {
+        private static String[] answers = new String[]
+       {
+            "Даже не думай",
+            "Определенно ДА",
+            "Можешь быть уверен в этом",
+            "Да!",
+            "Нет!",
+            "По моим данным — «нет»",
+            "Весьма сомнительно",
+            "Сейчас нельзя предсказать",
+            "Спроси позже",
+            "Пока не ясно, попробуй снова",
+            "Знаки говорят — «да»",
+            "Хорошие перспективы",
+            "Сконцентрируйся и спроси опять"
+       };
+        public static String GetRandom()
+        {
+            return answers[new Random().Next(answers.Length)];
+        }
+    }
+}
